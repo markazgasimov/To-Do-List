2e5bbe9d68b9e7ea85ba011dad9f1b3d5542fd4b
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-07-16 00:12:51 +0000

    Finished

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 "use strict";
 $(document).ready(function () {
 
+$('.btn').click(function (e) { 
+    e.preventDefault();
+    let func = a =>{
+        if(Number(a)){
+            for(let i=1; i<=a; i++){
+                $('.content').append(`<h1>${a}</h1>`)
+            }
+        }else if(String(a)){
+            $('.content').append(`<h1>${a}</h1>`)
+        }else if($('input').val('')){
+            $('.content').append("")
+        }
+        $('input').val('');
+    }
+    func($('input').val());
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });

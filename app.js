6b99edaca227be377b7c27f8278311050d8e760d
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-07-26 08:40:34 +0000

    end

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 "use strict";
 $(document).ready(function () {
-
-// $('.btn').click(function (e) { 
-//     e.preventDefault();
-//     let func = a =>{
-//         if(Number(a)){
-//             for(let i=1; i<=a; i++){
-//                 $('.content').append(`<h1>${a}</h1>`)
-//             }
-//         }else if(String(a)){
-//             $('.content').append(`<h1>${a}</h1>`)
-//         }else if($('input').val('')){
-//             $('.content').append("")
-//         }
-//         $('input').val('');
-//     }
-//     func($('input').val());
-// });
-
 $('.btn').click(function (e) { 
     e.preventDefault();
-    function func(myclass = "") {
-        if (!$.trim($("input").val()) == '') {
-            let value = $('<h3></h3>')
-        }else{
-            return
-        }        
+    let func = a =>{
+        if(Number(a)){
+            for(let i=1; i<=a; i++){
+                $('.content').append(`<h1>${a}</h1>`)
+            }
+        }else if(String(a)){
+            $('.content').append(`<h1>${a}</h1>`)
+        }else if($('input').val('')){
+            $('.content').append("")
+        }
+        $('input').val('');
     }
-    
-    
+    func($('input').val());
 });
-
-
 });

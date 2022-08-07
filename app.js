9bba1782e1d4e5e2b3ee5475f475c01e806f1084
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-08-07 08:50:06 +0000

    FINISH PROJECT

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ $('.btn').click(function (e) {
     e.preventDefault();
     if (!$.trim($('input').val()) == '') {
         let a = b => {
-            for (let i = 0; i < b; i++) { $('.content').append(`<h3>${b}</h3>`) };
+            for (let i = 0; i < b; i++) { $('.content').append(`<h2>${b}</h2>`) };
         }
-        let d = f => Number(f) ? a(f) : $('.content').append(`<h3>${f}</h3>`);
+        let d = f => Number(f) ? a(f) : $('.content').append(`<h2>${f}</h2>`);
         d($('input').val());
     }else{
         return

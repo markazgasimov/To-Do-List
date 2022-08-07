2b4bd39a27d62179d0da26e936ae9980618f6fdd
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-08-07 08:37:33 +0000

    FINISH

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 "use strict";
 $(document).ready(function () {
-// $('.btn').click(function (e) { 
-//     e.preventDefault();
-//     let func = a =>{
-//         if(Number(a)){
-//             for(let i=1; i<=a; i++){
-//                 $('.content').append(`<h1>${a}</h1>`)
-//             }
-//         }else if(String(a)){
-//             $('.content').append(`<h1>${a}</h1>`)
-//         }else if($('input').val('')){
-//             $('.content').append("")
-//         }
-//         $('input').val('');
-//     }
-//     func($('input').val());
-// });
-// });
 
 $('.btn').click(function (e) {
     e.preventDefault();
     if (!$.trim($('input').val()) == '') {
         let a = b => {
-            for (let i = 0; i < b; i++) { $('.minibox').append(`<h3>${b}</h3>`) };
+            for (let i = 0; i < b; i++) { $('.content').append(`<h3>${b}</h3>`) };
         }
-        let d = f => Number(f) ? a(f) : $('.minibox').append(`<h3>${f}</h3>`);
+        let d = f => Number(f) ? a(f) : $('.content').append(`<h3>${f}</h3>`);
         d($('input').val());
+    }else{
+        return
     }
     $('input').val('') 
 })
-
 
 });
 
